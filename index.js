a6a7dea8d1e411d4c15fe128f3019be2a183a946
Vladimir Starkov <iamstarkov@gmail.com>
2016-05-02 18:34:44 +0200

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import esDepsFromString from 'es-deps-from-string';
 import R from 'ramda';
 import pify from 'pify';
@@ -5,14 +6,16 @@ import Promise from 'pinkie-promise';
 import _fs from 'fs';
 import contract from 'neat-contract';
 
-const fs = pify(_fs, Promise);
+// readFile :: String -> Promise
+const readFile = R.curryN(2, pify(_fs.readFile, Promise));
 
-const resolve = Promise.resolve.bind(Promise);
+// toPromise :: a -> Promise a
+const toPromise = Promise.resolve.bind(Promise);
 
 // esDeps :: String -> Promise Array[String]
-const esDeps = R.binary(R.pipeP(resolve,
+const esDeps = R.unary(R.pipeP(toPromise,
   contract('file', String),
-  file => fs.readFile(file, 'utf8'),
+  readFile(R.__, 'utf8'),
   esDepsFromString
 ));
 
