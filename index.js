6dfcf78ce407e2b5dfda8c3355691358c88afa56
Vladimir Starkov <iamstarkov@gmail.com>
2016-06-29 13:25:13 +0200

    refactor

--- index.js
+++ index.js
@@ -3,29 +3,40 @@ import esDepsFromString from 'es-deps-from-string';
 import R from 'ramda';
 import pify from 'pify';
 import Promise from 'pinkie-promise';
-import _fs from 'fs';
+import fs from 'fs';
 import contract from 'neat-contract';
 
-// readFile :: String -> Promise
-const readFile = R.curryN(2, pify(_fs.readFile, Promise));
+const memoizedEsDepsFromString = R.memoize(esDepsFromString);
+
 
 // toPromise :: a -> Promise a
 const toPromise = Promise.resolve.bind(Promise);
 
-// parsing :: String :: String -> Promise [String]
-const parsing = file => m => Promise.resolve(m)
-  .then(R.memoize(esDepsFromString))
-  .catch(_err => {
-    const err = _err;
-    err.message += ` in ${file}`;
-    return Promise.reject(err);
-  });
+
+// readFile :: String -> Promise String
+const readFile = R.flip(pify(fs.readFile, Promise));
+
+
+// addFileNameToError :: String -> SyntaxError -> Promise.reject SyntaxError
+const addFileNameToError = R.curry((file, _err) => {
+  const err = _err;
+  err.message += ` in ${file}`;
+  return Promise.reject(err);
+});
+
+
+// parsing :: String -> String -> Promise [String]
+const parsing = R.curry((file, content) => Promise.resolve(content)
+  .then(memoizedEsDepsFromString)
+  .catch(addFileNameToError(file))
+);
+
 
 // esDeps :: String -> Promise [String]
 function esDeps(file) {
   return R.pipeP(toPromise,
     contract('file', String),
-    readFile(R.__, 'utf8'),
+    readFile('utf8'),
     parsing(file)
   )(file);
 }
--- test.js
+++ test.js
@@ -1,20 +1,21 @@
 import test from 'ava';
 import esDeps from './index';
 
-const expected = [
-  'out', './local',
-  'q', 'fs', './local-cjs', 'globalImport',
-];
+test('should esDeps', async t => {
+  const actual = await esDeps('./fixtures/index.js');
+  const expected = [
+    'out', './local',
+    'q', 'fs', './local-cjs', 'globalImport',
+  ];
+  t.deepEqual(actual, expected);
+});
 
-test('should esDeps', async t => t.deepEqual(
-  await esDeps('./fixtures/index.js'),
-  expected
-));
-
-test('throw decent error on bad files', t => t.throws(
-  esDeps('./fixtures/bad.js'),
-  'Unexpected token (1:0) in ./fixtures/bad.js'
-));
+test('throw decent error on bad files', t => {
+  t.throws(
+    esDeps('./fixtures/bad.js'),
+    'Unexpected token (1:0) in ./fixtures/bad.js'
+  );
+});
 
 test('empty input', t => t.throws(esDeps(), TypeError));
 test('invalid input', t => t.throws(esDeps(2), TypeError));
